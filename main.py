56c5280dfe26df483332a0b7e2aab597d55c7956
DESKTOP-TRHJ47V\torre <mario.iglesias@udc.es>
2018-12-16 21:04:39 +0100

    Mostrar por pantalla todos los pasos

--- main.py
+++ main.py
@@ -1,53 +1,39 @@
 import cv2
 import numpy as np
-import scipy.stats
 from scipy.signal import argrelextrema
 
 import positions
+import mostrarPasos as m
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from scipy import ndimage
 
-from skimage import data
-from skimage.filters import threshold_otsu
-from skimage.segmentation import clear_border
 from skimage.measure import label, regionprops
-from skimage.morphology import closing, square
 from skimage.color import label2rgb
 
-#interpolacion
-# regresion robusta
-#runsac
+
 def readImage (thigh, img):
     imgOr = cv2.imread(img, 0)
 
     imgTh = cv2.imread(img, 0)
-    imgTh = ndimage.grey_opening(imgTh, size=(3, 4))
 
-    imgTh = cv2.GaussianBlur(imgTh, (11, 11), 0)
-    #imgTh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    # imgTh =  cv2.getGaussianKernel()
-    # grad_y = cv2.Sobel(imgTh, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
-    # grad_x = cv2.Sobel(imgTh, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
-    #
-    # imgTh = cv2.convertScaleAbs(grad_y)
-    # abs_grad_x = cv2.convertScaleAbs(grad_x)
-    # abs_grad_y = cv2.convertScaleAbs(grad_y)
-    # imgTh = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+    opening = ndimage.grey_opening(imgTh, size=(3, 4))
 
-    imgTh[imgTh < thigh] = 0
-    imgTh[imgTh >= thigh] = 1
+    gauss = cv2.GaussianBlur(opening, (11, 11), 0)
+
+    imgTh[gauss < thigh] = 0
+    imgTh[gauss >= thigh] = 1
 
     kernel = np.ones((1, 5))
-    imgTh = cv2.erode(imgTh, kernel, iterations=1)
+    erode = cv2.erode(imgTh, kernel, iterations=1)
 
     kernel = np.ones((2,15))
-    imgDl = cv2.dilate(imgTh, kernel, iterations = 1)
+    dilate = cv2.dilate(erode, kernel, iterations = 1)
 
+    m.showImage6(imgOr, opening, gauss, imgTh, erode, dilate, "Original", "Apertura gris", "Suav Gauss", "Th", "Erode", "Dilate")
 
-
-    return imgOr, imgTh, imgDl
+    return imgOr, dilate
 
 def newImage(image, regionCoords):
     for i in regionCoords:
@@ -59,57 +45,38 @@ def cConexas(image):
     label_image = label(image)
     imageComponentesConexas = np.zeros(image.shape)
 
-    regions = []
+    regions1 = []
     for region in regionprops(label_image):
-        # take regions with large enough areas
         if region.area >= 1000:
-            regions.append(region)
+            regions1.append(region)
 
-    for region in regions:
+    for region in regions1:
         imageComponentesConexas = newImage(imageComponentesConexas, region.coords)
 
-
-
-
     kernel = np.ones((3, 9))
-    imageComponentesConexas = cv2.morphologyEx(imageComponentesConexas, cv2.MORPH_CLOSE, kernel)
+    imageComponentesConexasOpen = cv2.morphologyEx(imageComponentesConexas, cv2.MORPH_CLOSE, kernel)
 
+    label_image = label(imageComponentesConexasOpen)
+    imageComponentesConexas2 = np.zeros(image.shape)
 
-
-    label_image = label(imageComponentesConexas)
-    imageComponentesConexas = np.zeros(image.shape)
-
-    regions = []
+    regions2 = []
     for region in regionprops(label_image):
-        # take regions with large enough areas
         if region.area >= 2000:
-            regions.append(region)
+            regions2.append(region)
 
-    regions.sort(key=lambda x: x.area, reverse=True)
-    regions = sorted(regions, key=lambda x: x.area, reverse=True)
+    regions2.sort(key=lambda x: x.area, reverse=True)
+    regions = sorted(regions2, key=lambda x: x.area, reverse=True)
 
     regions = regions[0:5]
 
     for region in regions:
-        imageComponentesConexas = newImage(imageComponentesConexas, region.coords)
+        imageComponentesConexas2 = newImage(imageComponentesConexas2, region.coords)
 
-    return imageComponentesConexas, regions
+    m.showImage3(imageComponentesConexas, imageComponentesConexasOpen, imageComponentesConexas2, "CC Area 1000", "CC Close", "CC Area 2000")
 
-# def unionCC(regions):
-#
-#     for r in regions:
-#         minr1, minc1, maxr1, maxc1 = regions[r].bbox
-#         ltmp = []
-#         for i in range(len(regions)):
-#             minr2, minc2, maxr2, maxc2 = regions[i].bbox
-#             if(minc1==minc2 and minr1 == minr2 and maxc1== maxc2 and maxr1== maxr2 ): continue
-#             if (minr1<minr2 and maxr1> maxr2):
-#                 ltmp.append()
+    return imageComponentesConexas2, regions
 
-        
-
-
-def cConexas2(image, nombre):
+def cConexas2(image):
 
     label_image = label(image)
     image_label_overlay = label2rgb(image, image=image)
@@ -146,32 +113,15 @@ def cConexas2(image, nombre):
 
         imageComponentesConexas = newImage(imageComponentesConexas, region.coords)
 
-
     ax.set_axis_off()
     plt.tight_layout()
     plt.imshow(imageComponentesConexas, cmap='gray')
-    plt.title(nombre)
+    plt.title("CC Finales")
     return imageComponentesConexas, regions
-
-def showImage(ori, th,  dl, cc):
-    plt.figure()
-    plt.subplot(221), plt.imshow(ori, cmap='gray')
-    plt.title('OR Image'), plt.xticks([]), plt.yticks([])
-
-    plt.subplot(222), plt.imshow(th, cmap='gray')
-    plt.title('th Image'), plt.xticks([]), plt.yticks([])
-
-    plt.subplot(223), plt.imshow(dl, cmap='gray')
-    plt.title('DL Image'), plt.xticks([]), plt.yticks([])
-
-    plt.subplot(224), plt.imshow(cc, cmap='gray')
-    plt.title('CC Image'), plt.xticks([]), plt.yticks([])
-    plt.show()
 
 def combine(original, regions):
     filas, columnas = original.shape
 
-    # outImage = np.zeros((filas,columnas))
     outImage = original.copy()
     for i in range(filas):
         for j in range(columnas):
@@ -179,12 +129,6 @@ def combine(original, regions):
                 outImage[i][j] = original[i][j]
             else:
                 outImage[i][j] =0
-
-    # kernel = np.ones((3,4))
-    # print(kernel)
-    # outImage =  cv2.morphologyEx(outImage, cv2.MORPH_CLOSE, kernel)
-    # outImage = cv2.Canny(outImage, 120, 180)
-
 
     return outImage
 
@@ -214,15 +158,6 @@ def countPx(img):
         dist.append(positions.Positions(i, posInit, posInit + countR1))
         valueComunas[i]= countR1
 
-
-    # print(dist[0].colum)
-    plt.figure()
-    plt.imshow(out, cmap='gray')
-    plt.title('Parts'), plt.xticks([]), plt.yticks([])
-
-    # print("Nº total de px: ",totalPX)
-    # print("Media PX: ", totalPX/columnas)
-
     return out, dist
 
 def calculateDist(dist):
@@ -242,22 +177,12 @@ def calculateDist(dist):
     for i in  range(len(distancias)):
         dist[i].distanciaSuav = distancias[i]
 
-    plt.figure()
-    plt.plot(distancias)
-    plt.title("SUAVIZADAAAAAA")
-
-    plt.figure()
-    plt.plot(listDist)
-    plt.title("SIIIIINNNNN")
-    desviacionTipica(dist)
+    dt , ml = desviacionTipica(dist)
+    return listDist, distancias, dt, ml
 
 def reduceBorders(img):
     filas, columnas = img.shape
     out = np.zeros((filas, columnas))
-
-    # plt.figure()
-    # plt.imshow(img, cmap='gray')
-    # plt.title('IMG'), plt.xticks([]), plt.yticks([])
 
     for i in range(columnas):
         blanco = True
@@ -267,10 +192,6 @@ def reduceBorders(img):
             if((not blanco) and img[j][i] == 255):
                 out[j][i] = 255
                 blanco= True
-    plt.figure()
-    plt.imshow(out, cmap='gray')
-    plt.title('Reduc'), plt.xticks([]), plt.yticks([])
-
     return out
 
 def desviacionTipica(dist):
@@ -316,7 +237,6 @@ def desviacionTipica(dist):
                 valuePrin = fifteenPercent
                 valueFin =fivePercent
     else :
-        print("EEEEEEQQQQQQQQ")
         valuePrin = 0
         valueFin = 0
     print(valuePrin)
@@ -340,12 +260,11 @@ def desviacionTipica(dist):
     localMaxim =argrelextrema(tmp, np.greater)
     print(localMaxim[0])
 
-    plt.figure()
-    plt.plot([f.distanciaSuav for f in dist])
-    plt.title("DESVIACION TIPICA")
 
-    unionMaxLocal(localMaxim[0], dist)
-    # print([f.distanciaSuav for f in dist])
+    desviacionTipica= [f.distanciaSuav for f in dist]
+    union = unionMaxLocal(localMaxim[0], dist)
+
+    return desviacionTipica, union
 
 def unionMaxLocal(localMax, points):
     sizePoints = int(len(points))
@@ -355,7 +274,6 @@ def unionMaxLocal(localMax, points):
     for i in range(sizePoints):
         if (not negativos):
             if(points[i].distanciaSuav == -1):
-                print(points[i].distanciaSuav)
                 negativos= True
                 if(i > localMax[sizeMax-1]):
                     localAntSig.append([localMax[sizeMax-1], localMax[sizeMax-1]])
@@ -376,9 +294,8 @@ def unionMaxLocal(localMax, points):
     for i in range(len(localAntSig)):
         points = lineFromPoints(localAntSig[i], localMax, points)
 
-    plt.figure()
-    plt.plot([f.distanciaSuav for f in points])
-    plt.title("CREAR LINEAS")
+    union = [f.distanciaSuav for f in points]
+    return union
 
 
 def lineFromPoints(localAntSig, localMax, points):
@@ -391,37 +308,21 @@ def lineFromPoints(localAntSig, localMax, points):
     c = a * (P[0]) + b * (P[1])
 
     if (localAntSig[0] != localAntSig[1]):
-        print("DENTROOOOOOOOO")
         for i in range(localAntSig[0], localAntSig[1]):
             points[i].distanciaSuav =  (c - a*i)/b
     else:
-        print("MAAAAAAAAAAAAAL")
-
         if(localMax[0] == localAntSig[1]):
-            print("PRINCIPIOOOOO")
             for i in range(localAntSig[1]):
                 points[i].distanciaSuav = points[localAntSig[0]].distanciaSuav
         elif(localMax[len(localMax)-1] == localAntSig[1]):
-            print("FIIIIIIN")
-
             for i in range(localAntSig[1],len(points)):
                 points[i].distanciaSuav = points[localAntSig[0]].distanciaSuav
 
     return points
-    # if (b < 0):
-    #     print("The line passing through points P and Q is:",
-    #           a, "x ", b, "y = ", c, "\n")
-    # else:
-    #     print("The line passing through points P and Q is: ",
-    #           a, "x + ", b, "y = ", c, "\n")
-
-
 
 def reduceMiddleBorder(img, regions):
-    # print(regions[1].coords)
     tmp = np.zeros(img.shape)
     a = newImage(tmp, regions[1].coords)
-    # print(regions[1].coords)
     a = combine(img, a)
     filas, columnas = a.shape
     for i in range(columnas):
@@ -435,7 +336,6 @@ def reduceMiddleBorder(img, regions):
                 c = i
             a[j][i] =0
         if(f != 0):
-            # img[f][c] = 255
             tmp[f][c] = 255
 
     for i in regions[1].coords:
@@ -445,182 +345,32 @@ def reduceMiddleBorder(img, regions):
         for j in range(columnas):
             if(tmp[i][j] == 255):
                 img[i][j]=255
-    plt.figure()
-    plt.imshow(img, cmap='gray')
-    plt.title('MIDDLE'), plt.xticks([]), plt.yticks([])
-
     return img
 
 
-def execute(th):
-    # impTodas(th)
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im12.jpeg')
+def execute(imagen):
+    imgOr, imgDl = readImage(80, imagen)
     imgCc, _ = cConexas(imgDl)
-
-
-    imgCc, regiones = cConexas2(imgCc, "IMG 4")
-
-
-
-    # imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im9.jpeg')
-    # imgCc, _ = cConexas(imgDl)
-    # imgCc, regiones = cConexas2(imgCc, "IMG 9")
-    #
-    # imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im12.jpeg')
-    # imgCc, _ = cConexas(imgDl)
-    # imgCc, regiones = cConexas2(imgCc, "IMG 12")
-
-
-    # plt.figure()
-    # plt.imshow(imgDl, cmap='gray')
-    # plt.title('D1'), plt.xticks([]), plt.yticks([])
+    imgCc, regiones = cConexas2(imgCc)
 
     imgCombine = combine(imgOr, imgCc)
-    # a(imgCombine)
-
     rmb = reduceMiddleBorder(imgCombine,regiones)
+    thHigh = rmb.copy()
+    thHigh[rmb > 120] = 255
+    thHigh[rmb  <= 120] = 0
+    imgReduc = reduceBorders(thHigh )
 
-    rmb[rmb > 120] = 255
-    rmb[rmb  <= 120] = 0
-    imgReduc = reduceBorders(rmb )
+    m.showImage4(imgCombine, rmb, thHigh, imgReduc, "OR + Reg", "R mid edge", "ThHigh", "R All Edges")
 
-    imgFin, _ = countPx(imgReduc)
-    imgFin, capa1 = countPx(imgFin)
-    imgFin, capa2 = countPx(imgFin)
+    imgReg1, _ = countPx(imgReduc)
+    imgReg2, capa1 = countPx(imgReg1)
+    imgReg3, capa2 = countPx(imgReg2)
 
-    calculateDist(capa2)
-    # desviacionTipica(capa1)
+    m.showImage3(imgReg1, imgReg2, imgReg3, "PX R1", "PX R2", "PX R3")
 
+    distOr, distSuv, distDP, distML = calculateDist(capa2)
 
+    m.showStadistics(distOr, distSuv, distDP, distML, "Original", "Blur", "Desv Tipica", "Union")
 
-    # mediaTruncada(capa1)
-    # mediaTruncada(capa2)
-
-
-    # imagem = cv2.bitwise_not(imgCombine)
-    # imgFin = cConexas(imagem, True)
-
-    # plt.figure()
-    # plt.plot(imgOr[:, 0])
-
-    # imgFin = countPx(imgCombine)
-    # imgFin = countPx(imgFin)
-    # imgFin = countPx(imgFin)
-    # imgFin = countPx(imgFin)
-
-    # edges = cv2.Canny(imgCombine, 0, 50)
-    # kernel = np.ones((2, 1))
-
-    # erode = cv2.morphologyEx(edges, cv2.MORPH_DILATE, kernel)
-    # tmp = countPx(edges)
-
-
-
-
-    # plt.figure()
-    # plt.imshow(imgCombine, cmap='gray')
-    # plt.title('comb2'), plt.xticks([]), plt.yticks([])
-
-
-
-    # showImage(imgOr, imgTh, imgDl, imgCc)
-
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im2.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im3.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im4.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im5.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im6.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im7.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im8.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im9.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im10.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im11.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-    #
-    # imgOr, imgTh = readImage(th, 'AS-OCT\im12.jpeg')
-    # imgCc = cConexas(imgTh)
-    # showImage(imgOr, imgTh, imgCc)
-
-
-
-
-def impTodas(th):
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im1.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 1")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im2.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 2")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im3.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 3")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im4.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 4")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im5.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 5")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im6.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 6")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im7.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 7")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im8.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 8")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im9.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 9")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im10.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 10")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im11.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 11")
-
-    imgOr, imgTh, imgDl = readImage(th, 'AS-OCT\im12.jpeg')
-    imgCc, _ = cConexas(imgDl)
-    imgCc, regiones = cConexas2(imgCc, "IMG 12")
-
-execute(80)
+execute('AS-OCT\im12.jpeg')
 plt.show()
---
+++ mostrarPasos.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+
+
+def showImage6(img1, img2,  img3, img4, img5, img6, title1 = None, title2 = None, title3 = None, title4 = None, title5 = None, title6 = None):
+    plt.figure()
+    plt.subplot(321), plt.imshow(img1, cmap='gray')
+    plt.title(title1), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(322), plt.imshow(img2, cmap='gray')
+    plt.title(title2), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(323), plt.imshow(img3, cmap='gray')
+    plt.title(title3), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(324), plt.imshow(img4, cmap='gray')
+    plt.title(title4), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(325), plt.imshow(img5, cmap='gray')
+    plt.title(title5), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(326), plt.imshow(img6, cmap='gray')
+    plt.title(title6), plt.xticks([]), plt.yticks([])
+
+def showImage3(img1, img2,  img3, title1 = None, title2 = None, title3 = None):
+    plt.figure()
+    plt.subplot(131), plt.imshow(img1, cmap='gray')
+    plt.title(title1), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(132), plt.imshow(img2, cmap='gray')
+    plt.title(title2), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(133), plt.imshow(img3, cmap='gray')
+    plt.title(title3), plt.xticks([]), plt.yticks([])
+
+
+def showImage4(img1, img2,  img3, img4, title1 = None, title2 = None, title3 = None, title4 = None):
+    plt.figure()
+    plt.subplot(221), plt.imshow(img1, cmap='gray')
+    plt.title(title1), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(222), plt.imshow(img2, cmap='gray')
+    plt.title(title2), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(223), plt.imshow(img3, cmap='gray')
+    plt.title(title3), plt.xticks([]), plt.yticks([])
+
+    plt.subplot(224), plt.imshow(img4, cmap='gray')
+    plt.title(title4), plt.xticks([]), plt.yticks([])
+
+def showStadistics(st1, st2,  st3, st4, title1 = None, title2 = None, title3 = None, title4 = None):
+    plt.figure()
+    plt.plot(st1)
+    plt.title(title1)
+
+    plt.plot(st2)
+    plt.title(title2)
+
+    plt.plot(st3)
+    plt.title(title3)
+
+    plt.plot(st4)
+    plt.title(title4)
